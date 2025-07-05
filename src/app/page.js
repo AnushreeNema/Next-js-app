fbbff6fb2256cedc27f676b0701d27367cfc3be8
Anushree Nema <anushreenema624@gmail.com>
2025-07-05 18:15:48 +0530

    links to login and about page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
-"use client"; // always on type of file, on the child files of this file do not have to apply use client automatically applied
+import Link from "next/link";
 import styles from "./page.module.css";
-import { useState } from "react";
-//Error: Event handlers cannot be passed to Client Component props.
-
-//If you need interactivity, consider converting part of this to a Client Component.
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Basic routing</h1>
+      <Link href="/login">Go to login page</Link>
+      <Link href="/about">Go to about page</Link>
     </main>
   );
 }
