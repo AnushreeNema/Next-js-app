491bbbd791b72f5c91acb46ea900f834e0f0ada2
Anushree Nema <anushreenema624@gmail.com>
2025-07-05 18:24:27 +0530

    userouter

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,19 @@
+"use client";
 import Link from "next/link";
 import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
   return (
     <main className={styles.main}>
       <h1>Basic routing</h1>
       <Link href="/login">Go to login page</Link>
+      <br />
+      <br />
       <Link href="/about">Go to about page</Link>
+      <br />
+      <br />
+      <button onClick={() => router.push("/login")}>Go to login Page </button>
     </main>
   );
 }
