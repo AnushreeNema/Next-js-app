eec29afe5216335c387f1860b9a2b2ac6aefc7a1
Anushree Nema <anushreenema624@gmail.com>
2025-07-05 17:36:44 +0530

    event handlers cannot be on client

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+//Error: Event handlers cannot be passed to Client Component props.
 
+//If you need interactivity, consider converting part of this to a Client Component.
 export default function Home() {
   return (
     <main className={styles.main}>
-      <User name="Anil" />
-      <User name="peter" />
-      <User name="sameer" />
-      <h1>Home page</h1>
+      <h1>Events, function and state</h1>
+      <button onClick={() => alert("hello next js")}>Click me</button>
     </main>
   );
 }
