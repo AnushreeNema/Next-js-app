03426cad8c5ec1afd447a30f0b9f3922a8c366e8
Anushree Nema <anushreenema624@gmail.com>
2025-07-05 17:46:44 +0530

    usestate

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,18 @@
 "use client"; // always on type of file, on the child files of this file do not have to apply use client automatically applied
 import styles from "./page.module.css";
+import { useState } from "react";
 //Error: Event handlers cannot be passed to Client Component props.
 
 //If you need interactivity, consider converting part of this to a Client Component.
 export default function Home() {
-  const apple = (item) => {
-    alert(item);
+  const [name, setName] = useState("anil");
+  const apple = () => {
+    setName("sidhu");
   };
   return (
     <main className={styles.main}>
-      <h1>Events, function and state</h1>
-      <button onClick={() => apple("color")}>Click me</button>
+      <h1>Events, function and state {name}</h1>
+      <button onClick={() => apple()}>Click me</button>
     </main>
   );
 }
