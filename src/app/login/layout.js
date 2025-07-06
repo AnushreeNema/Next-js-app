e61d14542be4fe757630bfa55eb4bdfa03701b37
Anushree Nema <anushreenema624@gmail.com>
2025-07-06 11:50:30 +0530

    conditional layout

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,24 +1,31 @@
+"use client";
 import Link from "next/link";
 import "./login.css";
-
+import { usePathname } from "next/navigation";
 export default function Layout({ children }) {
+  const pathName = usePathname();
+  console.log(pathName);
   return (
     <div>
-      <h1>Common layout for login screens</h1>
-      <ul className="login-menu">
-        <li>
-          <h4>Login navbar</h4>
-        </li>
-        <li>
-          <Link href={"/login"}>Login Main</Link>
-        </li>
-        <li>
-          <Link href="/login/loginstudent">Student Login</Link>
-        </li>
-        <li>
-          <Link href="/login/loginteacher">Teacher Login</Link>
-        </li>
-      </ul>
+      {pathName !== "/login/loginteacher" ? (
+        <ul className="login-menu">
+          <li>
+            <h4>Login navbar</h4>
+          </li>
+          <li>
+            <Link href={"/login"}>Login Main</Link>
+          </li>
+          <li>
+            <Link href="/login/loginstudent">Student Login</Link>
+          </li>
+          <li>
+            <Link href="/login/loginteacher">Teacher Login</Link>
+          </li>
+        </ul>
+      ) : (
+        <Link href="/login">Go to main login page</Link>
+      )}
+
       {children}
     </div>
   );
